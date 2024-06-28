f6acdb57520ac2ce280f5d8cf59920bfde4ddf43
Stas Malyshev <malishev.stanislav@yandex.ru>
2024-06-28 11:44:07 +0500

    add switch

--- Les1Ts3.php
+++ Les1Ts3.php
@@ -1,16 +1,42 @@
 <?php
-$variable = true;
+$input = 1;
 
-if (is_bool($variable)) {
+if (is_bool($input)) {
     echo 'bool';
-} else if (is_int($variable)) {
+} else if (is_int($input)) {
     echo 'int';
-} else if (is_float($variable)) {
+} else if (is_float($input)) {
     echo 'float';
-} else if (is_string($variable)) {
+} else if (is_string($input)) {
     echo 'string';
-} else if (is_null($variable)) {
+} else if (is_null($input)) {
     echo 'null';
 } else {
     echo 'other';
+}
+
+?>
+
+<?php
+$input = '1';
+
+switch (true) {
+    case is_bool($input):
+        echo 'bool';
+        break;
+    case is_float($input):
+        echo 'float';
+        break;
+    case is_int($input):
+        echo 'int';
+        break;
+    case is_string($input):
+        echo 'string';
+        break;
+    case is_null($input):
+        echo 'null';
+        break;
+    default:
+        echo 'other';
+        break;
 }
